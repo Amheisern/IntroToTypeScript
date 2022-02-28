524419ac182121d5ea412506d43c9cdfa77993ce
lodashreload <icarusjapan@gmail.com>
2022-02-27 19:23:47 -0500

    start to some typescript code

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,12 @@
-import "./style.css";
+import './style.css'
+let score = 100
+
+if (score == 100) {
+  console.log('Win')
+}
+
+const personName = 'mary'
+
+if (personName == 'mary') {
+  console.log('nice')
+}
